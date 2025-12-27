872a7598b3e3b23bd7fbf5ac3436227e766a5cd0
Aljunalei Alfonso <154396730+Junalei@users.noreply.github.com>
2025-12-27 13:16:40 +0800

    Chore: added messafe response after message forward success

--- autobooks-ph/src/app/api/contact/route.ts
+++ autobooks-ph/src/app/api/contact/route.ts
@@ -5,11 +5,11 @@ import { NextResponse } from 'next/server';
 export async function POST(request: Request) {
   try {
     const data = await request.json();
-    
+
     console.log('API received contact form data:', data);
 
-     //N8N webhook connection
-    const response = await fetch('https://alju.app.n8n.cloud/webhook/autobooks-contact-form',{
+    //N8N webhook connection
+    const response = await fetch('https://alju.app.n8n.cloud/webhook/autobooks-contact-form', {
       method: 'POST',
       headers: {
         contentType: 'application/json'
@@ -18,18 +18,18 @@ export async function POST(request: Request) {
     });
 
     console.log('N8N response status:', response.status)
-  // Error handling for N8N response
+    // Error handling for N8N response
     if (!response.ok) {
-      console.log('N8N returned error:',  response.statusText);
-      return NextResponse.json({error: 'Failed to forward message'}, {status: response.status});
-  }
+      console.log('N8N returned error:', response.statusText);
+      return NextResponse.json({ error: 'Failed to forward message' }, { status: response.status });
+    }
+    return NextResponse.json({ message: 'Message forwarded successfully' }, { status: 200 });
 
-  
   }
   catch (error) {
     console.log('API fetching error:', error);
-    return NextResponse.json({error: 'Failed'}, {status: 500});
+    return NextResponse.json({ error: 'Failed' }, { status: 500 });
   }
- 
+
 }
 
